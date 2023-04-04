0be547823095332486c4d6a1473bb3dade41f573
Fumegalli <rafael.fumegalli@cwi.com.br>
2023-04-04 19:18:23 -0300

    feat: :sparkles: add "mark as read" feature

--- src/components/todo-item/TodoItem.tsx
+++ src/components/todo-item/TodoItem.tsx
@@ -1,18 +1,23 @@
+import { Todo } from '../../App';
 import { DeleteIcon } from '../delete-icon/DeleteIcon';
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
 
 import './TodoItem.css'
 
 type Props = {
-    text: string,
-    id: string,
-    onClickDelete: Function
+    todo: Todo;
+    onClickDelete: (id: string) => void;
+    onClickComplete: (id: string) => void;
 }
 
-export function TodoItem({ text, id, onClickDelete }: Props) {
+export function TodoItem({ todo, onClickDelete, onClickComplete }: Props) {
     return (
-        <div className='todo-item' key={id}>
-            <div className='todo-text'>{text}</div>
-            <button className='delete-icon' onClick={() => onClickDelete(id)}>
+        <div className='todo-item' key={todo.id}>
+            <button className='checkbox-container' onClick={() => onClickComplete(todo.id)}>
+                {todo.isCompleted ? <CheckCircleRoundedIcon /> : <div />}
+            </button>
+            <div className={todo.isCompleted ? 'todo-text-completed' : 'todo-text'}>{todo.text}</div>
+            <button className='delete-icon' onClick={() => onClickDelete(todo.id)}>
                 <DeleteIcon />
             </button>
         </div>
